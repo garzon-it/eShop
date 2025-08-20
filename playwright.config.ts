f7e64fd12af689c895990d4fb18a8c6f97403d74
Andres Garzon <andres@garzon-it.de>
2025-08-20 17:56:44 +0200

    test3

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://localhost:19888',
+    ignoreHTTPSErrors: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
