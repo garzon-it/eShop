fb1f77a4ee61c42daf878d6b5fe984f9ebae3695
Andres Garzon <andres@garzon-it.de>
2026-01-20 18:20:06 +0100

    increase playwright test timeout.

--- playwright.config.ts
+++ playwright.config.ts
@@ -94,6 +94,6 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
     stderr: 'pipe',
     stdout: 'pipe',
-    timeout: process.env.CI ? (5 * 60_000) : 60_000,
+    timeout: process.env.CI ? (15 * 60_000) : (3 * 60_000),
   },
 });
