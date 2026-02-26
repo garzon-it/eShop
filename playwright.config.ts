0cbe2279f9b9fa168fd552f8bf47005a2eed9212
Andres Garzon <andres@garzon-it.de>
2026-02-26 19:02:16 +0100

    Playwright tests: upload junit xml as artifact.

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit', { outputFile: 'results.xml' }]
+    ['junit', { outputFile: 'artifacts/test-results/results.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
