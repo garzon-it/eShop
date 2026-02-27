a4fa273b615f249881069f5774943e9715cd6b2f
Andres Garzon <andres@garzon-it.de>
2026-02-27 18:51:33 +0100

    fix BasketServiceTests intentionally failing.

--- tests/Basket.UnitTests/BasketServiceTests.cs
+++ tests/Basket.UnitTests/BasketServiceTests.cs
@@ -22,7 +22,7 @@ public class BasketServiceTests
         var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
 
         Assert.IsInstanceOfType<CustomerBasketResponse>(response);
-        Assert.AreEqual(response.Items.Count(), 1); // ! TODO andres: set 1 -> 0. Was intentionally modified to generate an error.
+        Assert.AreEqual(response.Items.Count(), 0); // ! TODO andres: set to 1 if test should fail.
     }
 
     [TestMethod]
