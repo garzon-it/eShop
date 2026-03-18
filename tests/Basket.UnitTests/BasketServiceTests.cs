f893b541c495f55a7d0b0f0d7eb5597ddcec234a
Andres Garzon <andres@garzon-it.de>
2026-03-18 13:49:31 +0100

    revert test failed on purpose.

--- tests/Basket.UnitTests/BasketServiceTests.cs
+++ tests/Basket.UnitTests/BasketServiceTests.cs
@@ -22,7 +22,7 @@ public class BasketServiceTests
         var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
 
         Assert.IsInstanceOfType<CustomerBasketResponse>(response);
-        Assert.AreEqual(response.Items.Count(), 1); // ! TODO andres: set to 1 if test should fail.
+        Assert.AreEqual(response.Items.Count(), 0); // ! TODO andres: set to 1 if test should fail.
     }
 
     [TestMethod]
@@ -40,7 +40,7 @@ public class BasketServiceTests
         var response = await service.GetBasket(new GetBasketRequest(), serverCallContext);
 
         Assert.IsInstanceOfType<CustomerBasketResponse>(response);
-        Assert.AreEqual(response.Items.Count(), 2);
+        Assert.AreEqual(response.Items.Count(), 1);
     }
 
     [TestMethod]
